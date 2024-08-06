34bcb4773e51898905f61daac3b8ee72929076f3
Sahariar Sagor <contactsahariar@gmail.com>
2024-08-06 12:10:35 +0600

    Digital Memo

---
+++ Cash memo/script.js
@@ -0,0 +1,27 @@
+// finding element 
+const cashForm = document.getElementById("cashForm");
+const productName = document.getElementById("productName");
+const productPrice = document.getElementById("productPrice");
+const totalPrice = document.getElementById("totalPrice");
+const result = document.getElementById("result");
+
+//create operator function
+
+const createList = (x, y) =>{
+    const paragraph = document.createElement("p");
+    result.appendChild(paragraph);
+    paragraph.innerHTML = `Product Name: <b>${x}</b> the price is: <b>${y}</b>`;
+}
+
+let total = 0;
+cashForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const inputPrice = parseFloat(productPrice.value);
+    total += inputPrice;
+    totalPrice.textContent = total;
+
+    createList(productName.value, productPrice.value);
+    
+    productPrice.value = "";
+    productName.value = "";
+})
